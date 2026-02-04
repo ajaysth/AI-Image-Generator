94e35ffa46816a9b43507c60055e6bb8ac0c4ceb
ajaysth <shrestha11ajay@gmail.com>
2026-02-04 11:50:47 +0545

    designs enhanced even more

--- src/components/ImageGenerator/ImageGenerator.tsx
+++ src/components/ImageGenerator/ImageGenerator.tsx
@@ -1,6 +1,6 @@
 import default_image from "../assets/messi.jpg"
 import bg_image from "../assets/bg.jpg"
-import { FaSearch } from "react-icons/fa";
+import { RiAiGenerate } from "react-icons/ri";
 
 
 const ImageGenerator = () => {
@@ -38,16 +38,18 @@ const ImageGenerator = () => {
                         alt=""
                     />
 
-                    <div className="relative">
-                        {/* Input */}
-                        <input
-                            type="text"
-                            placeholder="Describe your image..."
-                            className=" w-full rounded-md bg-black/30 border border-white/20 text-white px-4 py-2 outline-none focus:ring-2 focus:ring-cyan-400"
+                    <div className="relative w-full">
+  <input
+    type="text"
+    placeholder="Describe your image..."
+    className="w-full h-12 rounded-full bg-black/30 border border-white/20 text-white pl-4 pr-32 outline-none focus:ring-2 focus:ring-cyan-400"
+  />
 
-                        />
-                        <FaSearch className="absolute right-4 top-1/2 -translate-y-1/2 text-white/70 pointer-events-none" />
-                    </div>
+  <button className="absolute right-1 top-1/2 -translate-y-1/2 h-10 px-4 flex items-center gap-2 text-sm font-semibold text-white bg-blue-600 rounded-full cursor-pointer hover:bg-blue-700 transition">
+    Generate
+    <RiAiGenerate className="text-lg" />
+  </button>
+</div>
 
 
                 </div>
